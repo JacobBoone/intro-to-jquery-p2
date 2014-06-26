3a4f25c27ff6a2290ad7f72cf9139f50a846c3d4
Jacob <jakeboone@aol.com>
2014-06-26 17:10:18 -0600

    Completed 1-3 of p2

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 $(document).on('ready', function() {
   
+	
+
+	$("p").mouseover(function(){
+  		$(this).css("color","hotpink");
+		console.log('i am now pink');
+	});
+
+
+	$("p").mouseout(function(){
+  		$(this).css("color","#000");
+		console.log('i am now the default color');
+	});
+
+
+	$("h1").append("!");
+		console.log('added a ! to headers');
+
+
+
+	$("a").click(function(){
+  	
+
+		console.log('i am now restored');
+	});
+
+	// $('a').click(confirm('Do you want to visit this page?'))'
+
 });
